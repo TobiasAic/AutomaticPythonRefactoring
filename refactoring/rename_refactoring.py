b8dcf6d7285d43d07a438015de975f66b23fe570
TobiasAichele <st187455@stud.uni-stuttgart.de>
2026-06-16 17:32:12 +0200

    fix: prevent rope from creating the .ropeproject directory that makes problems

--- refactoring/rename_refactoring.py
+++ refactoring/rename_refactoring.py
@@ -9,7 +9,8 @@ class RenameRefactoring(Refactoring):
     def __init__(self, filepath: str, offset: int, new_name: str):
         super().__init__(filepath)
 
-        self.project = Project(os.path.dirname(filepath))
+        # ropefolder=None stops rope from creating a .ropeproject folder, which helps to keep the project directory clean
+        self.project = Project(os.path.dirname(filepath), ropefolder=None)
         self.resource = libutils.path_to_resource(self.project, filepath)
         self.rename = Rename(self.project, self.resource, offset)
         self.changes = self.rename.get_changes(new_name)
