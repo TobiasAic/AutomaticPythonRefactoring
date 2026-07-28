a8831363ab4a665909f488fb7cd1b96cf76426e0
Topas <st187455@stud.uni-stuttgart.de>
2026-07-28 13:07:39 +0200

    feature: add LLMs to presets

--- llm/llm_presets.py
+++ llm/llm_presets.py
@@ -14,3 +14,21 @@ qwen3_5_2b_config = LLMConfig(
     base_url="http://localhost:11434/v1",
     model_name="qwen3.5:2b"
 )
+
+qwen3_6_35b_config = LLMConfig(
+    api_key="supersecret", # dummy value, Ollama does not require an API key
+    base_url="http://localhost:11434/v1",
+    model_name="qwen3.6:35b"
+)
+
+qwen3_7_plus_config = LLMConfig(
+    api_key=get_env_variable("OPENCODE_GO_API_KEY"),
+    base_url="https://opencode.ai/zen/go/v1/",
+    model_name="qwen3.7-plus"
+)
+
+deepseek_v4_pro_config = LLMConfig(
+    api_key=get_env_variable("OPENCODE_GO_API_KEY"),
+    base_url="https://opencode.ai/zen/go/v1/",
+    model_name="deepseek-v4-pro"
+)
