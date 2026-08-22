61aaa748f70c3893f2d194a0a9ab598cde877df4
Topas <st187455@stud.uni-stuttgart.de>
2026-08-22 18:56:54 +0200

    chore: cleanup presets

--- llm/llm_presets.py
+++ llm/llm_presets.py
@@ -3,32 +3,25 @@
 from llm.openai_llm import LLMConfig
 from utility.environment_variables import get_env_variable
 
-big_pickle_config = LLMConfig(
+"""
+Example LLM configuration for Ollama models:
+ollama_model_config = LLMConfig(
+    api_key="supersecret", # dummy value, Ollama does not require an API key
+    base_url="http://localhost:11434/v1",
+    model_name="model_name"
+)
+
+Example LLM configuration for OpenCode models:
+opencode_model_config = LLMConfig(
     api_key=get_env_variable("OPENCODE_API_KEY"),
     base_url="https://opencode.ai/zen/v1",
-    model_name="big-pickle"
+    model_name="model_name"
 )
-
-qwen3_5_2b_config = LLMConfig(
-    api_key="supersecret", # dummy value, Ollama does not require an API key
-    base_url="http://localhost:11434/v1",
-    model_name="qwen3.5:2b"
-)
-
-qwen3_6_35b_config = LLMConfig(
-    api_key="supersecret", # dummy value, Ollama does not require an API key
-    base_url="http://localhost:11434/v1",
-    model_name="qwen3.6:35b"
-)
+For this to work, you need to set the OPENCODE_API_KEY environment variable in your .env file.
+"""
 
 qwen3_7_plus_config = LLMConfig(
     api_key=get_env_variable("OPENCODE_GO_API_KEY"),
     base_url="https://opencode.ai/zen/go/v1/",
     model_name="qwen3.7-plus"
-)
-
-deepseek_v4_pro_config = LLMConfig(
-    api_key=get_env_variable("OPENCODE_GO_API_KEY"),
-    base_url="https://opencode.ai/zen/go/v1/",
-    model_name="deepseek-v4-pro"
 )
